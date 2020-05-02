741bc2a0dd43f05e9b384aa9effa9e1c770f665a
Juan Jose <j.lopez2@estudiantes.matcom.uh.cu>
2020-05-02 14:43:49 -0400

    add jwtGenerator and userAccessor in  Security folder

---
+++ Reactivities/Reactivities.Infraestructure/Security/JwtGenerator.cs
@@ -0,0 +1,43 @@
+﻿using Reactivities.Application.Interfaces;
+using Reactivities.Domain;
+using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Reactivities.Infraestructure.Security
+{
+    public class JwtGenerator : IJwtGenerator
+    {
+        private readonly SymmetricSecurityKey _key;
+        public JwtGenerator(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+        public string CreateToken(AppUser user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            //generate signing credentials
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var creds = new  SigningCredentials(_key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
---
+++ Reactivities/Reactivities.Infraestructure/Security/UserAccessor.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Reactivities.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace Reactivities.Infraestructure.Security
+{
+    public class UserAccessor : IUserAccesssor
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public UserAccessor(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string GetCurrentUserName()
+        {
+            var username = _httpContextAccessor.HttpContext.User?.Claims?.FirstOrDefault( x => 
+                x.Type == ClaimTypes.NameIdentifier)?.Value;
+            return username;
+        }
+    }
+}
