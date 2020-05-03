591827ce8d291415cce3512b3987aa020187a17f
Juan Jose <j.lopez2@estudiantes.matcom.uh.cu>
2020-05-03 12:59:29 -0400

    Add attend and unattend commands

---
+++ Reactivities/Reactivities.Application/Activities/Attend.cs
@@ -0,0 +1,67 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Reactivities.Application.Errors;
+using Reactivities.Application.Interfaces;
+using Reactivities.Domain;
+using Reactivities.Persistence;
+using System;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reactivities.Application.Activities
+{
+    public class Attend
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccesssor _userAccessor;
+
+            public Handler(DataContext context, IUserAccesssor userAccessor)
+            {
+                _context = context;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                    throw new RestException(HttpStatusCode.NotFound,
+                        new { Activity = "Could not find activity" });
+
+                var user = await _context.Users.SingleOrDefaultAsync(x =>
+                   x.UserName == _userAccessor.GetCurrentUserName());
+
+                var attendance = await _context.UserActivities.SingleOrDefaultAsync(x =>
+                    x.AppUserId == user.Id && x.ActivityId == activity.Id);
+
+                if(attendance != null)
+                    throw new RestException(HttpStatusCode.BadRequest,
+                        new { Attendance = "Already attending this activity" });
+
+                attendance = new UserActivity
+                {
+                    AppUser = user,
+                    Activity = activity,
+                    IsHost = false,
+                    DateJoined = DateTime.Now
+                };
+
+                _context.UserActivities.Add(attendance);
+
+                var succes = await _context.SaveChangesAsync() > 0;
+
+                if (succes) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
---
+++ Reactivities/Reactivities.Application/Activities/Unattend.cs
@@ -0,0 +1,64 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Reactivities.Application.Errors;
+using Reactivities.Application.Interfaces;
+using Reactivities.Domain;
+using Reactivities.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Reactivities.Application.Activities
+{
+    public class Unattend
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+        }
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IUserAccesssor _userAccessor;
+
+            public Handler(DataContext context, IUserAccesssor userAccessor)
+            {
+                _context = context;
+                _userAccessor = userAccessor;
+            }
+
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                    throw new RestException(HttpStatusCode.NotFound,
+                        new { Activity = "Could not find activity" });
+
+                var user = await _context.Users.SingleOrDefaultAsync(x =>
+                   x.UserName == _userAccessor.GetCurrentUserName());
+
+                var attendance = await _context.UserActivities.SingleOrDefaultAsync(x =>
+                    x.AppUserId == user.Id && x.ActivityId == activity.Id);
+
+                if (attendance == null)
+                    return Unit.Value;
+
+                if(attendance.IsHost)
+                    throw new RestException(HttpStatusCode.BadRequest,
+                        new { Attendance = "You cannot remove yourself as host" });
+
+                _context.UserActivities.Remove(attendance);
+
+                var succes = await _context.SaveChangesAsync() > 0;
+
+                if (succes) return Unit.Value;
+
+                throw new Exception("Problem saving changes");
+            }
+        }
+    }
+}
