0ad47b623e73c3758d9b46c5bede27f783d57106
Juan Jose <j.lopez2@estudiantes.matcom.uh.cu>
2020-05-03 12:58:51 -0400

    Add AutoMapper Profile

---
+++ Reactivities/Reactivities.Application/Activities/ActivityDto.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Reactivities.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace Reactivities.Application.Activities
+{
+    public class ActivityDto
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public DateTime Date { get; set; }
+        public string City { get; set; }
+        public string Venue { get; set; }
+
+        [JsonProperty("attendees")]
+        public ICollection<AttendeeDto> UserActivities { get; set; }
+    }
+}
---
+++ Reactivities/Reactivities.Application/Activities/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Reactivities.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Reactivities.Application.Activities
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Activity, ActivityDto>();
+            CreateMap<UserActivity, AttendeeDto>()
+                .ForMember(d => d.UserName, o => o.MapFrom(s => s.AppUser.UserName))
+                .ForMember(d => d.DisplayName, o => o.MapFrom(s => s.AppUser.DisplayName));
+        }
+    }
+}
